bd816bf122be776cddd381f7dd201adf459a6d12
Mohabexe <s22512065eg@std.aou.edu.eg>
2024-03-04 14:01:49 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        hanken: ["Hanken Grotesk", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
